aae3fe14554d836a4e391affef13e7fcbe4e2763
s-allam <saeedallam527@outlook.com>
2024-01-10 04:00:10 -0800

    2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     mYset = set(my_list)
-    count = mYset[0]
-    for i  in mYset:
-        if i != mYset[0]:
-            count +=i
-    return count
+    return sum(mYset)
