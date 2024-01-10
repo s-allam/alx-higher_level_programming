7986728693320e0d1be560aeab6f37ba995dc97b
s-allam <saeedallam527@outlook.com>
2024-01-10 09:03:04 -0800

    0x04-python-more_data_structures32

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    return(set_1 & set_2)
+   common_elements = set()
+   for item in set_1:
+       if item in set_2:
+           common_elements.add(item)
+   return common_elements
